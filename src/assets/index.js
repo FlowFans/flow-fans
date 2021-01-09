20c5920c83ff9c0b2de84fccc16a0a012924ae06
Caos <caos@Caoss-iMac.local>
2021-01-09 20:11:42 +0800

    add bigdipper

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,13 +2,15 @@ import Flow from './flow.svg';
 import Forum from './Forum.png';
 import FlowScan from './flowscan.svg'
 import Blocto from './Blocto.png'
+import Bigdipper from './Bigdipper.png'
 
 
 const maps = {
   Flow,
   Forum,
   FlowScan,
-  Blocto
+  Blocto,
+  Bigdipper
 }
 
 export default maps
