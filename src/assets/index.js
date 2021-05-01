34a27c08adeb0fdaf5dbf3e7266ee5c6d51d3d48
Caos <caosbad@gmail.com>
2021-05-01 17:17:37 +0800

    feat: add content

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,10 @@ import Bigdipper from './Bigdipper.png'
 import Bisontrails from './Bisontrails.svg'
 import NBA from './NBA.svg'
 import VIV3 from './viv3.png'
+import DarkC from './DarkC.jpeg'
+import Versus from './Versus.png'
+import Docs from './Docs.png'
+import Monster from './Monster.png'
 
 
 const maps = {
@@ -16,7 +20,11 @@ const maps = {
   Bigdipper,
   Bisontrails,
   NBA,
-  VIV3
+  VIV3,
+  DarkC,
+  Versus,
+  Docs,
+  Monster
 }
 
 export default maps
